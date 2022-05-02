6e2424e9759214b0194a832f68738eff0331840c
Alekhya-prog <85741825+Alekhya-prog@users.noreply.github.com>
2022-05-02 18:59:52 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,6 @@
+function user_ls ()
+{
+userName=document.getElementById ("input1").value;
+localStorage.setItem ("name" , userName);
+window.location = "kwitter_room.html";
+}
---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABJHeeCpEf1i8L8K6E--BYSIZy4UDB8vE",
+    authDomain: "kwitter-80e98.firebaseapp.com",
+    databaseURL: "https://kwitter-80e98-default-rtdb.firebaseio.com",
+    projectId: "kwitter-80e98",
+    storageBucket: "kwitter-80e98.appspot.com",
+    messagingSenderId: "1059856101629",
+    appId: "1:1059856101629:web:211f1f1ce9dc59ee5f0e94",
+    measurementId: "G-SRYS9B2ENW"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+  
